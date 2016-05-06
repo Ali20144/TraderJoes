aebd1a1c71312bcfdee084a64db5a9cfc599b139
Ali20144 <asghara674@gmail.com>
2016-05-06 07:23:43 +0000

    finalise

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+import os
 app = Flask(__name__)
+
+import sqlite3
+from flask import g
+
+DATABASE = 'database.db'
+
+
+
+#http://flask.pocoo.org/docs/0.10/patterns/sqlite3/
+def get_db():
+    def connect_to_database():
+        db = sqlite3.connect(DATABASE)
+        
+        db.execute('create table if not exists testimonials (id integer primary key, fname text, lname text, email text, phone text, messagebox text)')
+        
+        return db
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
@@ -25,7 +52,32 @@ def usedcars():
     
 @app.route('/testimonials')
 def testimonials():
-    return render_template('testimonials.html')
+    
+    db = get_db()
+    
+    cursor = db.cursor()
+    rows = cursor.execute('select fname,lname,messagebox from testimonials')
+    
+    return render_template('testimonials.html', rows=rows)
+
+@app.route('/testimonial-submit')
+def testimonialsubmit():
+    
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    messagebox = request.args.get('messagebox')
+    
+    db = get_db()
+    
+    cursor = db.cursor()
+    
+    cursor.execute('insert into testimonials (fname,lname,email,phone,messagebox) values (?,?,?,?,?)', [fname,lname,email,phone,messagebox])
+    
+    db.commit()
+    
+    return render_template('testimonials-submit.html')
 
 @app.route('/contactus')
 def contactus():
@@ -33,4 +85,4 @@ def contactus():
 
    
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0', debug=True)
+    app.run(port=int(os.getenv('PORT', 8080)), host=os.getenv('IP','0.0.0.0'), debug=True)
